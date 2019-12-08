518d694b7a588a55de500e24525c77d9aaa4e8cb
Alexander <ashuaibu@getcarbon.co>
2019-12-08 19:22:13 +0100

    [add] turn await to promise

--- functions/routes/election.js
+++ functions/routes/election.js
@@ -4,18 +4,26 @@ var router=express.Router();
 
 // a route to get all the current elections in our blockchain
 router.use("/",async (request,response)=>{
-    
+    // wrap it in a try catch in case there are any errors
     try{
-        const elections=[]
-        const electionsLength = await electionFactory.methods.electionsLength().call()
-        for(let i=0;i<electionsLength;i++){
-            let newElection=await electionFactory.methods.summaries(i).call()
-            elections.push(newElection)
-        }
-        response.json(elections);
+        // get an array that determines the length of elections we have
+        const electionsLength = await electionFactory.methods.getDeployedElections().call()
+        // for each election we have get the details of said election
+        const electionDetails = await  Promise.all(electionsLength.map((_,index)=>{
+            return electionFactory.methods.summaries(index).call()
+        }))
+        // if no error was encountered, then we can go on and return the details
+        return response.json({
+            status:"success",
+            data:electionDetails
+        });
     }
     catch(err){
-        response.end(err.message)
+        // if any error was encountered return with the message
+        return response.json({
+            status:"error",
+            message:err.message
+        });
     }
 })
 
